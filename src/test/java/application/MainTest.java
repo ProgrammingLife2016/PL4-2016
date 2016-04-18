d30948ac6f503437922e962da9368d2e8827c0bb
Arthur Breurkes <a.r.breurkes@student.tudelft.nl>
2016-04-18 15:37:59 +0200

    Added files to initialize hierarchy for the project on the repository.

---
+++ src/test/java/application/MainTest.java
@@ -0,0 +1,28 @@
+package application;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Test class for Main.java.
+ */
+public class MainTest {
+
+    /**
+     * Test setup method.
+     * @throws Exception possible exception.
+     */
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    /**
+     * Test teardown method.
+     * @throws Exception possible exception.
+     */
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
