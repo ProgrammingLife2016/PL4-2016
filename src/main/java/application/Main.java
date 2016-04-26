b0a228ef792d8aedd3820cd88af5eaefdaa3bc11
Ties Westerborg <t.d.westerborg@student.tudelft.nl>
2016-04-26 12:45:48 +0200

    Gfa Test file and a bfs for the nodes.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,14 +1,15 @@
 package application;
 
+import core.Node;
 import core.Parser;
 
-import java.io.File;
 import java.util.HashMap;
 
 /**
  * Main class for initialization purposes.
  */
 public class Main {
+	static HashMap nodeMap;
 
 	/**
 	 * Standard main method.
@@ -18,7 +19,29 @@ public class Main {
 	public static void main(String[] args) {
 
 		Parser parser = new Parser();
-		HashMap nodeMap = parser.readGFA("src/main/resources/TB10.gfa");
+		nodeMap = parser.readGFA("src/main/resources/TBTestFile.gfa");
+		//System.out.println(nodeMap.values());
 
-    }
+		Node root = (Node)nodeMap.get(1);
+		System.out.println("Root: " + root.getSequence());
+		dfs(root, 1,new boolean[nodeMap.size()]);
+		}
+
+	private static void dfs(Node n,int ni,boolean[] marked){
+		if(n == null && ni>0) return;
+
+		System.out.print(n.getSequence() + "\t");
+		marked[ni-1] = true;
+
+		//for every child
+		for(int i: n.getLinks())
+		{
+			//if childs state is not visited then recurse
+			if(!marked[i - 1])
+			{
+				dfs((Node)nodeMap.get(i),i,marked);
+				marked[i-1] =true;
+			}
+		}
+	}
 }
