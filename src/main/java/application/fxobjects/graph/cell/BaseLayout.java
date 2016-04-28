a88f6fa0e5d32ca921fe571310cf80a7b29aca5b
Ties Westerborg <t.d.westerborg@student.tudelft.nl>
2016-04-28 17:58:04 +0200

    Layout changes.

--- src/main/java/application/fxobjects/graph/cell/BaseLayout.java
+++ src/main/java/application/fxobjects/graph/cell/BaseLayout.java
@@ -13,6 +13,7 @@ public class BaseLayout extends CellLayout {
     private int currentX;
     private int currentY;
     private CellType lastType;
+    private int cellCount = 0;
 
     public BaseLayout(Graph graph, int offset) {
         this.currentX = 20;
@@ -22,8 +23,12 @@ public class BaseLayout extends CellLayout {
         this.graph = graph;
     }
 
+    /**
+     * This method aligns every node.
+     */
     public void execute() {
         List<Cell> cells = graph.getModel().getAllCells();
+
 
         for (Cell cell : cells) {
             switch (cell.getType()) {
@@ -32,13 +37,21 @@ public class BaseLayout extends CellLayout {
                         currentX += offset;
                     cell.relocate(currentX, 200);
                     currentY = 200;
+                    cellCount = 0;
                 case TRIANGLE:
-                        currentX += offset;
-                        currentY += offset;
+                        if(cellCount%2==0) {
+                            currentY += (cellCount+1)* offset;
+                            currentY += offset * 2;
+                        }
+                    else {
+                            currentY -= (cellCount+1) * offset;
+                            currentY -= offset * 2;
+                        }
+                    currentX += offset;
+                    cellCount++;
 
-                        currentY -= offset * 2;
                     cell.relocate(currentX, currentY);
-                        currentY += offset * 2;
+                        //currentY += offset * 2;
                 default:
                     break;
             }
