50d843888abf0a4b17ad6b546ed9ecbeb3036df1
NielsW-dev <n.warnars@student.tudelft.nl>
2016-05-12 21:29:00 +0200

    TreeLayout refactor.

--- src/main/java/application/fxobjects/phylogeny/TreeLayout.java
+++ src/main/java/application/fxobjects/phylogeny/TreeLayout.java
@@ -27,23 +27,62 @@ public class TreeLayout extends CellLayout{
     private static final int BASE_X = 0;
     private static final int BASE_Y = 0;
 
+    private HashMap<Integer, Cell> drawnCells;
+    private HashMap<Integer, Cell> undrawnCells;
+
     public TreeLayout(Model model, int offset) {
         this.currentX = BASE_X;
         this.currentY = BASE_Y;
         this.offset = offset;
         this.model = model;
+
+        drawnCells = new HashMap<Integer, Cell>();
+        undrawnCells = new HashMap<Integer, Cell>();
     }
 
     @Override
     public void execute() {
-        final int leafX = model.getTree().getHeight() * 100;
-        HashMap<Integer, Cell> drawnCells = new HashMap<Integer, Cell>();
-        HashMap<Integer, Cell> undrawnCells = new HashMap<Integer, Cell>();
+        int count = 0;
 
+        // Initially all nodes are undrawn
         for (Cell cell : model.getAllCells()) {
             undrawnCells.put(cell.getCellId(), cell);
         }
 
+        // draw all leaves to the right of the screen.
+        drawLeaves();
+
+        // Draw parent nodes and edges to the parent nodes
+        drawParents();
+
+    }
+
+    /**
+     * Draw the parent of already drawn nodes and connect them using edges.
+     */
+    private void drawParents() {
+        List<TreeNode> leaves = model.getTree().getLeaves(model.getTree().getRoot());
+        for (TreeNode leave : leaves) {
+            TreeNode parentNode = leave.parent();
+            Cell parent = undrawnCells.get(parentNode.getKey());
+
+            if (parentNode == null) { continue; }
+            if (parent == null) { continue; }
+
+            List<TreeNode> siblings = model.getTree().getLeaves(parentNode);
+            List<Cell> leavesAsCells = TreeNodesToCells(siblings);
+
+            if (siblings.size() == 2) {
+                processParent(parent, leavesAsCells.get(0), leavesAsCells.get(1));
+            }
+        }
+    }
+
+    /**
+     * relocate all leaves to the right of the screen
+     */
+    private void drawLeaves() {
+        final int leafX = model.getTree().getHeight() * 100;
 
         for (int i = 0; i < model.getAllCells().size(); i++) {
             Cell cell = undrawnCells.get(i);
@@ -51,94 +90,56 @@ public class TreeLayout extends CellLayout{
             TreeNode nodeByCell = model.getTree().getNodeByKey(cellId);
 
             if (nodeByCell.isLeaf()) {
-                currentY += offset;
-                cell.relocate(leafX, currentY);
-                undrawnCells.remove(cell.getCellId());
-                drawnCells.put(cell.getCellId(), cell);
+                relocateCell(cell, leafX, currentY += offset);
             }
         }
-
-        for (TreeNode leave : model.getTree().nodes) {
-            TreeNode parentNode = leave.parent();
-            if (parentNode == null) { continue; }
-            Cell parent = undrawnCells.get(parentNode.getKey());
-            if (parent == null) { continue; }
-
-            List<TreeNode> siblings = model.getTree().getLeaves(parentNode);
-            if (siblings.size() == 2) {
-                List<Cell> leavesAsCells = getLeavesAsCells(drawnCells, siblings);
-                processParent(model, drawnCells, undrawnCells, parent, leavesAsCells.get(0), leavesAsCells.get(1));
-            }
-        }
-
-        for (TreeNode leave : model.getTree().nodes) {
-            TreeNode parentNode = leave.parent();
-            if (parentNode == null) { continue; }
-            Cell parent = undrawnCells.get(parentNode.getKey());
-            if (parent == null) { continue; }
-
-            List<TreeNode> siblings = model.getTree().getLeaves(parentNode);
-            if (siblings.size() == 2) {
-                List<Cell> leavesAsCells = getLeavesAsCells(drawnCells, siblings);
-                processParent(model, drawnCells, undrawnCells, parent, leavesAsCells.get(0), leavesAsCells.get(1));
-            }
-        }
-
-
-
-
-        System.out.println("Num drawn cells: " + drawnCells.size());
-        System.out.println("Num undrawn cells: " + undrawnCells.size());
-        System.out.println("Num nodes in tree: " + model.getTree().nodes.size());
-    }
-
-    private Cell getMostLeftCell(List<Cell> cells) {
-        if (cells.size() == 0) { return null; }
-        Cell res = cells.get(0);
-
-        for (Cell cell : cells) {
-            if (cell.getLayoutX() < res.getLayoutX()) {
-                res = cell;
-            }
-        }
-        return res;
-    }
-
-    private List<Cell> getLeavesAsCells(HashMap<Integer, Cell> cells, List<TreeNode> leaves) {
-        List<Cell> leafCells = new ArrayList<Cell>();
-
-        for (int i = 0; i < leaves.size(); i++) {
-            Cell leaf = cells.get(leaves.get(i).getKey());
-            if (leaf != null) {
-                leafCells.add(leaf);
-            }
-        }
-
-        return leafCells;
     }
 
     /**
-     * Relocate the parent and add the edges from the parent to the two leaves.
-     * @param model
-     * @param drawnCells
-     * @param undrawnCells
-     * @param parent
-     * @param leaf1
-     * @param leaf2
+     * Convert a list of Tree Nodes to a list of their corresponding Cells.
+     * @param treeNodes
+     * @return
      */
-    private void processParent(Model model, HashMap<Integer, Cell> drawnCells,
-                               HashMap<Integer, Cell> undrawnCells,
-                               Cell parent, Cell leaf1, Cell leaf2) {
+    private List<Cell> TreeNodesToCells(List<TreeNode> treeNodes) {
+        List<Cell> cells = new ArrayList<Cell>();
 
-        int newX = (int)Math.min(leaf1.getLayoutX(), leaf2.getLayoutX()) - 50;
-        int newY = ((int)(leaf1.getLayoutY() + leaf2.getLayoutY()) / 2);
+        for (TreeNode treeNode : treeNodes) {
+            int id = treeNode.getKey();
+            Cell cell = model.getAllCells().get(id);
+            cells.add(cell);
+        }
 
-        parent.relocate(newX, newY);
+        return cells;
+    }
 
-        model.addEdge(parent.getCellId(), leaf1.getCellId(), 1);
-        model.addEdge(parent.getCellId(), leaf2.getCellId(), 1);
 
-        undrawnCells.remove(parent.getCellId());
-        drawnCells.put(parent.getCellId(), parent);
+    /**
+     * Relocate the parent and add the edges from the parent to the two leaves.
+     * @param parent
+     * @param child1
+     * @param child2
+     */
+    private void processParent(Cell parent, Cell child1, Cell child2) {
+
+        int newX = (int)Math.min(child1.getLayoutX(), child2.getLayoutX()) - 50;
+        int newY = ((int)(child1.getLayoutY() + child2.getLayoutY()) / 2);
+
+        model.addEdge(parent.getCellId(), child1.getCellId(), 1);
+        model.addEdge(parent.getCellId(), child2.getCellId(), 1);
+
+        relocateCell(parent, newX, newY);
+    }
+
+    /**
+     * Relocate a cell and mark it as 'drawn'.
+     * @param cell
+     * @param x
+     * @param y
+     */
+    private void relocateCell(Cell cell, int x, int y) {
+
+        cell.relocate(x, y);
+        undrawnCells.remove(cell.getCellId());
+        drawnCells.put(cell.getCellId(), cell);
     }
 }
