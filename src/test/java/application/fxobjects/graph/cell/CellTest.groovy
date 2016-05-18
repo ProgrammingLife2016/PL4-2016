bd82c58f0b0a7b903731fd0519a069195083c4c5
NielsW-dev <n.warnars@student.tudelft.nl>
2016-05-18 09:55:00 +0200

    Add Parser test, remove unworkable groovy tests.

--- src/test/java/application/fxobjects/graph/cell/CellTest.groovy
+++ src/test/java/application/fxobjects/graph/cell/CellTest.groovy
@@ -1,5 +1,7 @@
 package application.fxobjects.graph.cell
 
+import org.junit.Before
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
 import static org.mockito.Mockito.CALLS_REAL_METHODS
@@ -7,14 +9,19 @@ import static org.mockito.Mockito.CALLS_REAL_METHODS
 /**
  * Created by Ties on 11-5-2016.
  */
-class CellTest extends GroovyTestCase {
+public class CellTest {
     @Mock
-    public final Cell c = Mockito.mock(Cell.class, CALLS_REAL_METHODS)
+    public final Cell
 
+    @Before
     void setUp() {
-
+        c = Mockito.mock(Cell.class, CALLS_REAL_METHODS);
     }
 
+    /**
+     * Test the class constructor.
+     */
+    @Test
     void testConstructor() {
         assertNotNull(c);
     }
