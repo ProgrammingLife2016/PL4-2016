040c0110fec6d97aac8b2fbf1fa547faf678bfb4
dvantetering <D.A.L.vanTetering@student.tudelft.nl>
2016-05-23 16:08:41 +0200

    Fix Fourth bug: users had no permission to write snapshot to their chosen directory

--- src/main/java/application/controllers/GraphController.java
+++ src/main/java/application/controllers/GraphController.java
@@ -8,15 +8,24 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Screen;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.Buffer;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -32,6 +41,8 @@ public class GraphController extends Controller<ScrollPane> {
     private int maxWidth;
     private int maxHeight;
 
+    private String position;
+
     /**
      * Constructor method for this class.
      *
@@ -44,14 +55,14 @@ public class GraphController extends Controller<ScrollPane> {
     public GraphController(Graph g, Object ref, MainController m, int depth) {
         super(new ScrollPane());
         this.graph = g;
-        this.zoomController = new ZoomController();
+        this.zoomController = new ZoomController(this);
         this.maxWidth = 0;
         this.graphMouseHandling = new GraphMouseHandling(m);
         this.screenSize = Screen.getPrimary().getVisualBounds();
         this.maxHeight = (int) screenSize.getHeight();
         this.getRoot().setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         this.getRoot().setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-
+        this.position = "";
 
         this.getRoot().addEventFilter(ScrollEvent.SCROLL, event -> {
             if (event.getDeltaY() != 0) {
@@ -75,10 +86,13 @@ public class GraphController extends Controller<ScrollPane> {
         return graph;
     }
 
-    public ZoomController getZoomController() { return zoomController; }
+    public ZoomController getZoomController() {
+        return zoomController;
+    }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) { }
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 
     /**
      * Init method for this class.
@@ -101,7 +115,7 @@ public class GraphController extends Controller<ScrollPane> {
         for (Cell c : list) {
             graphMouseHandling.setMouseHandling(c);
         }
-        
+
         graph.endUpdate();
         GraphLayout layout = new GraphLayout(graph.getModel(), 20,
                 (int) (screenSize.getHeight() - 25) / 2);
@@ -128,18 +142,31 @@ public class GraphController extends Controller<ScrollPane> {
         return graph.getGenomes();
     }
 
+    public String getPosition() {
+        return position;
+    }
+
     /**
      * Method take a snapshot of the current graph.
+     *
      * @throws IOException Throw exception on write failure.
      */
     public void takeSnapshot() throws IOException {
-        WritableImage image = new WritableImage(maxWidth + 50,
-                (int) screenSize.getHeight());
-        WritableImage snapshot = this.getRoot().getContent().snapshot(
-                new SnapshotParameters(), image);
+        try {
+            WritableImage image = new WritableImage(maxWidth + 50,
+                    (int) screenSize.getHeight());
+            WritableImage snapshot = this.getRoot().getContent().snapshot(
+                    new SnapshotParameters(), image);
 
-        File output = new File("/snapshot.png");
-        output.deleteOnExit();
-        ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png", output);
+            String filePath = System.getProperty("user.home") + "/AppData/Local/Temp";
+            File output = new File(filePath);
+            File temp = File.createTempFile("snapshot", ".png", output);
+            ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png", temp);
+
+            position = temp.getName();
+            temp.deleteOnExit();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
