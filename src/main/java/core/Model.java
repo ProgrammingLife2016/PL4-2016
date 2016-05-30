7bb312957bd513443f77f2dfe3d4b61fba78a38b
skullyhoofd <ricardojongerius@gmail.com>
2016-05-30 12:28:19 +0200

    trying

--- src/main/java/core/Model.java
+++ src/main/java/core/Model.java
@@ -296,8 +296,8 @@ public class Model {
      */
     public void setLayout() {
         this.screenSize = Screen.getPrimary().getVisualBounds();
-        this.graphLayout = new GraphLayout(this, (int) (screenSize.getWidth() / 40),
-                (int) (screenSize.getHeight() - 25) / 2);
+        this.graphLayout = new GraphLayout(this, 35,
+                (int) (screenSize.getHeight() - 150) / 2);
 
         graphLayout.execute();
     }
