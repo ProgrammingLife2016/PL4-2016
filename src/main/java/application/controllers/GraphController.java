a318e827a221a4b727139f78af0fe725af8e80b5
skullyhoofd <ricardojongerius@gmail.com>
2016-06-01 17:00:31 +0200

    simplifies if statement

--- src/main/java/application/controllers/GraphController.java
+++ src/main/java/application/controllers/GraphController.java
@@ -81,11 +81,8 @@ public class GraphController extends Controller<ScrollPane> {
     public void focus(Cell prevClick) {
         prevClick.resetFocus();
         for (Cell c : graph.getModel().getAllCells()) {
-            if (c.getCellId() == prevClick.getCellId()) {
-                prevClick = c;
-                break;
-            }
-            if (c.getCellId() > prevClick.getCellId()) {
+            if ((c.getCellId() == prevClick.getCellId()) ||
+                    (c.getCellId() > prevClick.getCellId())) {
                 prevClick = c;
                 break;
             }
