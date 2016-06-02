085cc1927d7c3471cb3b6f2c30b0df2bf25aeacd
Arthur Breurkes <a.r.breurkes@student.tudelft.nl>
2016-06-02 14:51:17 +0200

    Now it actually works.

--- src/main/java/application/controllers/TreeController.java
+++ src/main/java/application/controllers/TreeController.java
@@ -437,8 +437,7 @@ public class TreeController extends Controller<ScrollPane> {
     }
 
     public void clearSelection() {
+        selectedStrains.forEach(this::revertCellHighlight);
         selectedStrains.clear();
-        modifyGraphOptions();
-        applyColorOnCells();
     }
 }
