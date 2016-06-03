63fb9e9cf4ad3e0db7ae6afd3d157f367db813f6
NielsW-dev <n.warnars@student.tudelft.nl>
2016-06-03 10:23:24 +0200

    Add annotation ID to string

--- src/main/java/core/Node.java
+++ src/main/java/core/Node.java
@@ -425,7 +425,8 @@ public class Node {
         StringBuilder sb = new StringBuilder();
 
         for (Annotation a : getAnnotations()) {
-            sb.append(format("- %s, spanning nodes: ", a.getDisplayNameAttr()));
+            sb.append(format("- %s (ID: %d), spanning nodes: ", a.getDisplayNameAttr(),
+                    a.getIdAttr()));
 
             if (a.getSpannedNodes().size() >= 1) {
                 String prefix = "";
