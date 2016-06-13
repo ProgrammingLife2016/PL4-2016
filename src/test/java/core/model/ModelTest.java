99351c78445c9496cf7f22c9619548eabe7181c5
NielsW-dev <n.warnars@student.tudelft.nl>
2016-06-13 15:17:55 +0200

    Extended model test

--- src/test/java/core/model/ModelTest.java
+++ src/test/java/core/model/ModelTest.java
@@ -2,20 +2,32 @@ package core.model;
 
 import application.fxobjects.Cell;
 import application.fxobjects.Edge;
+import application.fxobjects.graphCells.RectangleCell;
+import application.fxobjects.layout.GraphLayout;
+import core.annotation.Annotation;
 import core.graph.Node;
 import core.typeEnums.CellType;
 import core.typeEnums.EdgeType;
+import javafx.scene.layout.StackPane;
 import net.sourceforge.olduvai.treejuxtaposer.drawer.Tree;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 /**
+ * Test class for the model class
+ *
  * Created by Ties on 11-5-2016.
  */
 public class ModelTest {
@@ -55,7 +67,19 @@ public class ModelTest {
     }
 
     /**
-     * Test the getTree method.
+     * Tests the get/setGraphLayout methods.
+     */
+    @Test
+    public void testGraphLayout() {
+        Model m = new Model();
+        GraphLayout gl = new GraphLayout(m, 1, 1);
+        m.setGraphLayout(gl);
+
+        assertEquals(gl, m.getGraphLayout());
+    }
+
+    /**
+     * Tests the getTree method.
      */
     @Test
     public void testGetTree() {
@@ -67,7 +91,15 @@ public class ModelTest {
     }
 
     /**
-     * Test the getAddedCells method.
+     * Tests the getLevelMapsSize method.
+     */
+    @Test
+    public void testGetLevelMapsSize() {
+        Model m = new Model();
+        assertEquals(0, m.getLevelMapsSize());
+    }
+    /**
+     * Tests the getAddedCells method.
      */
     @Test
     public void testGetAddedCells() {
@@ -90,7 +122,7 @@ public class ModelTest {
     }
 
     /**
-     * Test the getAddedEdges method.
+     * Tests the getAddedEdges method.
      */
     @Test
     public void testGetAddedEdges() {
@@ -105,14 +137,14 @@ public class ModelTest {
     }
 
     /**
-     * Test the getAllEdges method.
+     * Tests the getAllEdges method.
      */
     @Test
     public void testGetAllEdges() {
         Model m = new Model();
         m.addCell(1, "", 1, CellType.RECTANGLE);
-        m.addCell(2, "", 1, CellType.RECTANGLE);
-        m.addCell(3, "", 1, CellType.RECTANGLE);
+        m.addCell(2, "", 2, CellType.RECTANGLE);
+        m.addCell(3, "", 3, CellType.RECTANGLE);
 
         m.addEdge(1, 2, 1, EdgeType.GRAPH);
         m.addEdge(2, 3, 1, EdgeType.GRAPH);
@@ -123,7 +155,40 @@ public class ModelTest {
     }
 
     /**
-     * Test the getLevelMaps method.
+     * Tests the getCellMap method.
+     */
+    @Test
+    public void testGetCellMap() {
+        Model m = new Model();
+        m.addCell(1, "", 1, CellType.RECTANGLE);
+
+        assertEquals(1, m.getCellMap().size());
+    }
+
+    @Test
+    public void testAnnotations() {
+        Model m = new Model();
+        Annotation a = new Annotation();
+        a.setSeqid("test");
+
+        m.setAnnotations(Collections.singletonList(a));
+        assertEquals("test", m.getAnnotations().get(0).getSeqid());
+    }
+
+    /**
+     * Tests the addCell method.
+     */
+    @Test
+    public void testAddCell2() {
+        Model m = new Model();
+        Cell cell = mock(Cell.class);
+
+        assertTrue(m.addCell(cell));
+        assertFalse(m.addCell(cell));
+    }
+
+    /**
+     * Tests the getLevelMaps method.
      */
     @Test
     public void testGetLevelMaps() {
@@ -140,15 +205,119 @@ public class ModelTest {
     }
 
     /**
-     * Test the addEdge method.
+     * Tests the addEdge method.
      */
     @Test
     public void testAddEdge() {
         Model m = new Model();
-        m.addCell(mock(Cell.class));
-        m.addEdge(1, 1, 1, EdgeType.GRAPH);
+        m.addCell(1, "", 1, CellType.RECTANGLE);
+        m.addCell(2, "", 2, CellType.RECTANGLE);
+
+        m.addEdge(1, 2, 1, EdgeType.GRAPH);
 
         List<Edge> edges = m.getAddedEdges();
-        assertEquals(0, edges.size());
+        assertEquals(1, edges.size());
+    }
+
+    /**
+     * Tests the getEdgeFromChild method.
+     */
+    @Test
+    public void testGetEdgeFromChild() {
+        Model m = new Model();
+        RectangleCell c1 = spy(new RectangleCell(1, 1));
+        RectangleCell c2 = spy(new RectangleCell(2, 2));
+        RectangleCell c3 = spy(new RectangleCell(3, 3));
+
+        m.addCell(c1);
+        m.addCell(c2);
+        m.addCell(c3);
+
+        Edge e1 = new Edge(c1, c2, 1, EdgeType.GRAPH);
+        Edge e2 = new Edge(c2, c3, 1, EdgeType.GRAPH);
+
+        m.addEdge(e1);
+        m.addEdge(e2);
+
+        m.merge();
+        assertNull(m.getEdgeFromChild(c2));
+    }
+
+    /**
+     * Tests the getEdgeFromParent method.
+     */
+    @Test
+    public void testGetEdgeFromParent() {
+        Model m = new Model();
+        RectangleCell c1 = spy(new RectangleCell(1, 1));
+        RectangleCell c2 = spy(new RectangleCell(2, 2));
+        RectangleCell c3 = spy(new RectangleCell(3, 3));
+
+        m.addCell(c1);
+        m.addCell(c2);
+        m.addCell(c3);
+
+        Edge e1 = new Edge(c1, c2, 1, EdgeType.GRAPH);
+        Edge e2 = new Edge(c2, c3, 1, EdgeType.GRAPH);
+
+        m.addEdge(e1);
+        m.addEdge(e2);
+
+        m.merge();
+        assertEquals(0, m.getEdgeFromParent(c2).size());
+    }
+
+    /**
+     * Tests the getMaxWidth method.
+     */
+    @Test
+    public void testGetMaxWidth() {
+        Cell c = mock(Cell.class);
+        GraphLayout gl = mock(GraphLayout.class);
+        when(gl.getMaxWidth()).thenReturn(new Double(42));
+
+        Model m = new Model();
+        m.setGraphLayout(gl);
+        assertEquals(42, m.getMaxWidth(), 0.0001);
+    }
+
+    /**
+     * Tests the addEdge method.
+     */
+    @Test
+    public void testAddEdge2() {
+        Model m = new Model();
+        Edge e = mock(Edge.class);
+
+        m.addEdge(e);
+        assertEquals(e, m.getAddedEdges().get(0));
+    }
+
+    /**
+     * Tests the getLeftMost method.
+     */
+    @Test
+    public void testGetRightMost() {
+        Cell c = mock(Cell.class);
+        GraphLayout gl = mock(GraphLayout.class);
+        when(gl.getRightMost()).thenReturn(c);
+
+        Model m = new Model();
+        m.setGraphLayout(gl);
+        assertEquals(c, m.getRightMost());
+    }
+
+    /**
+     * Tests the getLeftMost method.
+     */
+    @Test
+    public void testGetLeftMost() {
+        Cell c = mock(Cell.class);
+        GraphLayout gl = mock(GraphLayout.class);
+        when(gl.getLeftMost()).thenReturn(c);
+
+        Model m = new Model();
+        m.setGraphLayout(gl);
+        assertEquals(c, m.getLeftMost());
     }
 }
