a43ab453b4455be9c137357452ff970a8bbf4c10
NielsW-dev <n.warnars@student.tudelft.nl>
2016-06-14 12:31:20 +0200

    Update graph parser test

--- src/test/java/core/parsers/GraphParserTest.java
+++ src/test/java/core/parsers/GraphParserTest.java
@@ -28,41 +28,24 @@ public class GraphParserTest {
         p = new GraphParser();
     }
 
-//    /**
-//     * Test the readGFA method.
-//     */
-//    @Test
-//    public void testReadGFA() {
-//        HashMap<Integer, Node> map = new HashMap<>();
-//
-//        try {
-//            InputStream is = getClass().getResourceAsStream("/TestFiles/TBTestFile.gfa");
-//            map = p.readGFA(is);
-//            is.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        assertEquals(4, map.size());
-//
-//        for (int i = 1; i <= 4; i++) {
-//            Node n = map.get(i);
-//            assertEquals(i, n.getId());
-//            assertEquals(Character.toString((char) ('A' + i - 1)), n.getSequence());
-//            assertEquals(i, n.getzIndex());
-//            assertEquals("GENOME_1", n.getGenomes().get(0));
-//            assertEquals(Integer.valueOf(i + 1), n.getLinks().get(0));
-//        }
-//    }
 
     /**
-     * Tests the readGFAFromFile class.
+     * Tests the readGFAFromFile method.
      */
     @Test
     public void testReadGFAFromFile() {
         try {
-            assertEquals(4, p.readGFAFromFile("src/main/resources/TestFiles/TBTestFile.gfa").size());
-        } catch (IOException e) {
+            HashMap<Integer, Node> map = p.readGFAFromFile("src/main/resources/TestFiles/TBTestFile.gfa");
+            assertEquals(4, map.size());
+
+            for (int i = 1; i <= 4; i++) {
+                Node n = map.get(i);
+                assertEquals(i, n.getId());
+                assertEquals(Character.toString((char) ('A' + i - 1)), n.getSequence());
+                assertEquals(i, n.getzIndex());
+                assertEquals("GENOME_1", n.getGenomes().get(0));
+                assertEquals(Integer.valueOf(i + 1), n.getLinks().get(0));
+            }        } catch (IOException e) {
         }
     }
 }
