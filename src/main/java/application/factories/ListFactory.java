bdb79efe67866b0f1bc46dbb9a0b1e66ee151989
NielsW-dev <n.warnars@student.tudelft.nl>
2016-06-14 13:18:42 +0200

    Removed unused Factory classes.

--- src/main/java/application/factories/ListFactory.java
+++ src/main/java/application/factories/ListFactory.java
@@ -68,9 +68,6 @@ public class ListFactory {
         list.setPlaceholder(new Label("No Genomes Loaded."));
         list.prefHeightProperty().bind(listVBox.heightProperty());
         list.prefWidthProperty().bind(listVBox.widthProperty());
-
-//        list.getStylesheets().add("/css/list.css");
-//        list.getStyleClass().add("list-view");
     }
 
 
@@ -85,14 +82,7 @@ public class ListFactory {
         id = new Text();
         id.setText("Select Node to view info");
 
-//        infoList.getStylesheets().add("/css/list.css");
-//        infoList.getStyleClass().add("text-flow");
-
-//        id.getStyleClass().add("text");
-
         infoList.getChildren().addAll(id);
-
-
     }
 
     /**
