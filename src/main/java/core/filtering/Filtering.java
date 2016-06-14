fb897a40321718c9c4c29c9799ac66be414af5e4
NielsW-dev <n.warnars@student.tudelft.nl>
2016-06-14 12:45:38 +0200

    Adds Filtering test

--- src/main/java/core/filtering/Filtering.java
+++ src/main/java/core/filtering/Filtering.java
@@ -58,14 +58,6 @@ public class Filtering {
     }
 
     /**
-     * Check whether filters are applied.
-     * @return true or false.
-     */
-    public boolean isFiltering() {
-        return !filters.isEmpty();
-    }
-
-    /**
      * Return the selected genomes after filtering.
      * @return a list of selected genomes.
      */
